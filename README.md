77f6022f2fa8ab8516e2465d89374da1ef438c66
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-02-12 13:58:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Emoji-Notification-SlackBot
+
+Notify specific channels that an emoji has been added
+
+## requirements
+
+### `.env`
+
+| ENV | Description |
+| -------- | -------- |
+| SLACK_BOT_TOKEN |  Bot User OAuth Access Token<br />String beginning with `xoxb-` |
+| CHANNEL_ID | For sending notifications |
+
+## Debug
+
+```
+go run main.go
+ngrok http http://localhost:3000
+```
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+		"os"
+
+    "github.com/joho/godotenv"
+    "github.com/slack-go/slack"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+			log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	http.HandleFunc("/slack/events", func(w http.ResponseWriter, r *http.Request) {
+    var reqBody map[string]interface{}
+    err := json.NewDecoder(r.Body).Decode(&reqBody)
+    if err != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+    }
+
+    // URL検証イベントの処理
+    if reqBody["type"] == "url_verification" {
+			challenge := reqBody["challenge"].(string)
+			w.Header().Set("Content-Type", "text/plain")
+			w.Write([]byte(challenge))
+			return
+    }
+
+		// イベントコールバックの処理
+		if reqBody["type"] == "event_callback" {
+			eventData := reqBody["event"].(map[string]interface{})
+			eventType := eventData["type"].(string)
+			if eventType == "emoji_changed" {
+				api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+				channelID := os.Getenv("CHANNEL_ID")
+				name := eventData["name"].(string)
+				text := fmt.Sprintf("新しい絵文字が追加されました! `:%s:` :%s:", name, name)
+				_, _, err := api.PostMessage(channelID, slack.MsgOptionText(text, false))
+				if err != nil {
+					log.Printf("Failed to post message: %v", err)
+				}
+			}
+			w.Header().Set("Content-Type", "text/plain")
+			w.Write([]byte("OK"))
+		}
+	})
+
+	fmt.Println("Server listening")
+	http.ListenAndServe(":3000", nil)
+}
