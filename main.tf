9e8934ca1907a688352299b74ec16323a0e02d6d
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-02-14 00:28:31 +0900

    Fix: Close #1

--- main.tf
+++ main.tf
@@ -1,7 +1,76 @@
+################################
+# Variables
+################################
+
+variable aws_profile {}
+variable aws_region {}
+variable resource_prefix {}
+variable SLACK_BOT_TOKEN {}
+variable CHANNEL_ID {}
+
+################################
+# LambdaにアタッチするIAM Role
+################################
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "${var.resource_prefix}-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+################################
+# API GatewayにアタッチするIAM Role
+################################
+
+resource "aws_iam_role" "api_gateway_role" {
+  name               = "${var.resource_prefix}-apigateway-role"
+  assume_role_policy = data.aws_iam_policy_document.api_gateway_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_policy_logs" {
+  role       = aws_iam_role.api_gateway_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_policy_lambda" {
+  role       = aws_iam_role.api_gateway_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaRole"
+}
+
+data "aws_iam_policy_document" "api_gateway_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+  }
+}
+
+################################
+# Lambda
+################################
+
 resource "aws_lambda_function" "lambda" {
-  function_name    = "Emoji-Notification-SlackBot"
+  function_name    = "${var.resource_prefix}"
   filename         = "./lambda/archive/sample.zip"
-  role             = var.LAMBDA_ARN
+  role             = aws_iam_role.lambda_role.arn
   handler          = "sample.exe"
   runtime          = "provided.al2"
   environment {
@@ -33,4 +102,66 @@ data "archive_file" "lambda" {
   output_path = "./lambda/archive/sample.zip"
  
   depends_on = [null_resource.default]
+}
+
+resource "aws_lambda_permission" "api_gateway_invoke_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
+
+################################
+# API Gateway
+################################
+
+resource "aws_apigatewayv2_api" "api" {
+  name = "${var.resource_prefix}"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.lambda.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+  # credentials_arn = aws_iam_role.api_gateway_role.arn
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+
+resource "aws_apigatewayv2_deployment" "api_deployment" {
+  api_id      = aws_apigatewayv2_api.api.id
+  # デプロイメントのトリガーとしてルートの変更を利用
+  depends_on = [aws_apigatewayv2_route.lambda_route]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_apigatewayv2_stage" "api_stage" {
+  api_id        = aws_apigatewayv2_api.api.id
+  name          = "$default"
+  deployment_id = aws_apigatewayv2_deployment.api_deployment.id
+}
+
+
+data "aws_iam_policy_document" "api_gateway_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["execute-api:Invoke"]
+    resources = ["${aws_apigatewayv2_api.api.execution_arn}/*"]
+  }
 }
